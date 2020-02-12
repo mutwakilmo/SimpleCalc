85f34c5ec9cac04cba77629dfd2838ae6448f380
mutwakilmo <mutwakilmo@gmail.com>
2020-02-12 11:32:13 +0100

    debugger

---
+++ app/src/main/java/com/mutwakilmo/android/simplecalc/Calculator.java
@@ -0,0 +1,38 @@
+package com.mutwakilmo.android.simplecalc;
+
+/**
+ * Created by Mutwakil Mo on 12/02/2020
+ */
+public class Calculator {
+
+    // Available operations
+    public enum Operator {ADD, SUB, DIV, MUL}
+
+    /**
+     * Addition operation
+     */
+    public double add(double firstOperand, double secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    /**
+     * Subtract operation
+     */
+    public double sub(double firstOperand, double secondOperand) {
+        return firstOperand - secondOperand;
+    }
+
+    /**
+     * Divide operation
+     */
+    public double div(double firstOperand, double secondOperand) {
+        return firstOperand / secondOperand;
+    }
+
+    /**
+     * Multiply operation
+     */
+    public double mul(double firstOperand, double secondOperand) {
+        return firstOperand * secondOperand;
+    }
+}
